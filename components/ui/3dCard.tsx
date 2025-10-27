eceb04febf72b37f95915c2cd7f96f39efa1b0c6
Mustafa Hasanain <m7asanain@gmail.com>
2025-10-27 15:10:24 +0300

    fix: type error in 3dCard component

--- components/ui/3dCard.tsx
+++ components/ui/3dCard.tsx
@@ -102,8 +102,8 @@ export const CardItem = ({
   rotateZ = 0,
   ...rest
 }: {
-  as?: React.ElementType;
-  children: React.ReactNode;
+  as?: any;
+  children?: React.ReactNode;
   className?: string;
   translateX?: number | string;
   translateY?: number | string;
@@ -113,7 +113,7 @@ export const CardItem = ({
   rotateZ?: number | string;
   [key: string]: any;
 }) => {
-  const ref = useRef<HTMLDivElement>(null);
+  const ref = useRef<any>(null);
   const [isMouseEntered] = useMouseEnter();
 
   useEffect(() => {
