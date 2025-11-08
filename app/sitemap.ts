93074890d84e2c5eaf34a65b13e2e48175dbc6db
Mustafa Hasanain <m7asanain@gmail.com>
2025-11-08 19:25:40 +0300

    fix: add force-static to sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,6 +1,8 @@
 import { MetadataRoute } from 'next';
 import { locales, defaultLocale } from '@/i18n';
 
+export const dynamic = 'force-static';
+
 export default function sitemap(): MetadataRoute.Sitemap {
   const baseUrl = 'https://mustafahasanain.com';
 
