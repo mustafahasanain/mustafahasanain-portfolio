6d12cb66815befc84408731dbb7168f37a02a109
Mustafa Hasanain <m7asanain@gmail.com>
2025-11-16 23:28:58 +0300

    fix: restore accidentally removed work experience images

--- data/experience.ts
+++ data/experience.ts
@@ -7,6 +7,7 @@ export const workExperience: WorkExperience[] = [
     description:
       "Built responsive web apps using Next.js, JavaScript, and TypeScript, ensuring smooth user experiences and modern UI aesthetics.",
     className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
   },
   {
     id: 2,
@@ -14,6 +15,7 @@ export const workExperience: WorkExperience[] = [
     description:
       "I have developed backend systems with Node.js and Express.js, handling APIs, databases, and server-side logic.",
     className: "md:col-span-2",
+    thumbnail: "/exp2.svg",
   },
   {
     id: 3,
@@ -21,6 +23,7 @@ export const workExperience: WorkExperience[] = [
     description:
       "Created branding materials, marketing assets, and digital graphics using Photoshop, Illustrator, Figma, and Canva, delivering visually engaging and cohesive designs tailored to client needs.",
     className: "md:col-span-2",
+    thumbnail: "/exp3.svg",
   },
   {
     id: 4,
@@ -28,5 +31,6 @@ export const workExperience: WorkExperience[] = [
     description:
       "I have created custom WordPress websites, including a personal portfolio and a time scheduling app with tailored functionality and responsive design.",
     className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
   },
 ];
--- types/index.ts
+++ types/index.ts
@@ -18,6 +18,7 @@ export interface WorkExperience {
   title: string;
   description: string;
   className: string;
+  thumbnail: string;
 }
 
 export interface TechStack {
